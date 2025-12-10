18b41c2d0da874cc8b7e63ba00b17b9b70eccda6
Ayushmancodes-08 <patraayushman21@gmail.com>
2025-12-10 14:08:55 +0530

    cgnv

--- src/config/constants.js
+++ src/config/constants.js
@@ -5,7 +5,7 @@ module.exports = {
     BASE_HASHTAGS: "#cddclubpmec",
     HASHTAG_COUNT: "5-6",
     CRON_SCHEDULES: {
-        MORNING: '0 14 * * *',
+        MORNING: '10 14 * * *',  // 2:10 PM IST (testing)
         EVENING: '0 20 * * *'
     },
     PERSONA: {
--- src/core/scheduler.js
+++ src/core/scheduler.js
@@ -6,17 +6,23 @@ const logger = require('../utils/logger');
 function initScheduler() {
     logger.log("Initializing Scheduler...", 'SYSTEM');
 
+    // Use Asia/Kolkata timezone so schedules run at correct IST times
+    // (Render servers use UTC by default)
+    const cronOptions = {
+        timezone: "Asia/Kolkata"
+    };
+
     cron.schedule(CRON_SCHEDULES.MORNING, () => {
         logger.log("Triggering Morning Run", 'SYSTEM');
         runBot();
-    });
+    }, cronOptions);
 
     cron.schedule(CRON_SCHEDULES.EVENING, () => {
         logger.log("Triggering Evening Run", 'SYSTEM');
         runBot();
-    });
+    }, cronOptions);
 
-    logger.log("Scheduled jobs for: " + Object.values(CRON_SCHEDULES).join(' and '), 'SUCCESS');
+    logger.log("Scheduled jobs for: " + Object.values(CRON_SCHEDULES).join(' and ') + " (IST)", 'SUCCESS');
 }
 
 module.exports = { initScheduler };
